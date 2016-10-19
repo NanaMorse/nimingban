e9c652407ff434cde89268b45821b8ac602e7468
Morse_Guo <morse@xmind.net>
2016-10-19 20:47:43 +0800

    Change loading cover's style

--- src/components/tools/LoadingCover.tsx
+++ src/components/tools/LoadingCover.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, StyleSheet, Dimensions, ActivityIndicator } from 'react-native';
+import { View, StyleSheet, Text, ActivityIndicator } from 'react-native';
 import ViewStyle = __React.ViewStyle;
 
 const styles = StyleSheet.create({
@@ -11,23 +11,33 @@ const styles = StyleSheet.create({
     right: 0,
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'center'
+  } as ViewStyle,
+
+  indicatorWrapper: {
+    width: 70,
+    height: 70,
+    borderRadius: 5,
+    alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'rgba(0,0,0,0.6)'
+    backgroundColor: 'rgba(0, 0, 0, 0.6)'
   } as ViewStyle
 });
 
 
 interface LoadingCoverProps {
   show?: boolean;
-  message?: string;
 }
 
 class LoadingCover extends React.Component<LoadingCoverProps, {}> {
   render() {
     if (this.props.show) {
+      // todo react native's activityIndicator has some wrong about it's position
       return (
         <View style={styles.container}>
-          <ActivityIndicator size="large" color="#eee"/>
+          <View style={styles.indicatorWrapper}>
+            <ActivityIndicator size="large" color="#eee"/>
+          </View>
         </View>
       );
     } else {
