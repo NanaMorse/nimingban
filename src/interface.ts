e0a002c5d36ab8ae2ed1c299aab8a827126ed2ba
Morse_Guo <nana.morse@outlook.com>
2016-10-23 10:09:40 +0800

    specify some interface

--- src/interface.ts
+++ src/interface.ts
@@ -1,22 +1,57 @@
 import * as RNRF from 'react-native-router-flux';
 
+export interface forumData {
+  id: string
+  // the id of this forum's group
+  fgroup: string
+  sort: string
+
+  // if showName existed, display showName, otherwise display name
+  name: string
+  // showName contains HTML string
+  showName: string
+  msg: string
+
+  // current forum's reply interval, the value is in the unit of second
+  // todo need to be init
+  interval: string
+
+  createdAt: string
+  updateAt: string
+  status: string
+}
+
+export interface forumListData {
+  id: string
+  sort: string
+  name: string
+  forums: forumData[]
+}
+
 export interface replyData {
-  admin: '0' | '1',
-  img: string;
-  ext: string;
-  content: string,
-  email: string,
-  id: string,
-  name: string,
-  now: string,
+  id: string
+  // combine img string and ext info, you can get the image url
+  img: string
+  ext: string
+
+  // reply time
+  now: string
+
   userid: string
+  name: string
+  email: string
+  title: string
+
+  // reply content, with HTML string
+  content: string
+
+  admin: '0' | '1'
 }
 
 export interface postData extends replyData {
-  title: string;
-  replyCount: string;
+  replyCount: string
   replys: replyData[]
-}
+} 
 
 type props = Object;
 
